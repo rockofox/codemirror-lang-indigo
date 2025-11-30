4abc24a14eab1a753b16d3dc2919270c42dadb18
rockofox <ofelix@pm.me>
2025-11-30 20:43:06 +0100

    Fix grammar conflicts, add field access and external blocks

--- src/index.ts
+++ src/index.ts
@@ -3,15 +3,16 @@ import { LRLanguage, LanguageSupport, indentNodeProp, foldNodeProp, foldInside, 
 import { styleTags, tags as t } from "@lezer/highlight"
 
 const keywords = ["let", "as", "qualified", "if", "then", "else", "do", "end", "import", "from", "struct", "value", "trait", "for", "impl", "satisfies", "is", "when", "of", "external"];
+const specialTypes = ["Self", "IO", "Any"];
 
 export const IndigoLanguage = LRLanguage.define({
   parser: parser.configure({
     props: [
       indentNodeProp.add({
-        Application: delimitedIndent({ closing: ")", align: false })
+        DoBlock: delimitedIndent({ closing: "end", align: false })
       }),
       foldNodeProp.add({
-        Application: foldInside
+        DoBlock: foldInside
       }),
       styleTags({
         Boolean: t.bool,
@@ -24,12 +25,11 @@ export const IndigoLanguage = LRLanguage.define({
         ComparisonOperator: t.compareOperator,
         BooleanOperator: t.logicOperator,
         StringConcatOperator: t.arithmeticOperator,
-        "Definition/LValue/Identifier": t.definition(t.functionName),
-        "Application/BaseExpression/Identifier": t.functionName,
-        "Application/expression/Identifier": t.variableName,
+        "Definition/LValue/Identifier": t.definition(t.function(t.variableName)),
+        Identifier: t.variableName,
         "Type/Identifier": t.typeName,
         "GenericType/Identifier": t.typeName,
-        "ListType/Identifier": t.typeName,
+        List: t.typeName,
         "Parameter/Identifier": t.variableName,
         "Lambda/ParameterSection/Parameter/Identifier": t.variableName,
         "Import/Identifier": t.namespace,
@@ -39,14 +39,15 @@ export const IndigoLanguage = LRLanguage.define({
         "StructField/Identifier": t.variableName,
         "FieldAccess/Identifier": [t.variableName, t.propertyName],
         "Cast/Type/Identifier": t.typeName,
-        "isSection/Type/Identifier": t.typeName,
+        "isTypes/Type/Identifier": t.typeName,
         Number: t.number,
         Float: t.number,
         ListLiteral: t.squareBracket,
         "( )": t.paren,
         "[ ]": t.squareBracket,
         "{ }": t.brace,
-        ...(Object.fromEntries(keywords.map(kw => [kw, t.keyword])))
+        ...(Object.fromEntries(keywords.map(kw => [kw, t.keyword]))),
+        ...(Object.fromEntries(specialTypes.map(st => [st, t.typeName])))
       }),
     ]
   }),
